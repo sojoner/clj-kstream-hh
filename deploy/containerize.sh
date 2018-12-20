59e354438b37da43d5256faa75f796f68a7412cc
Sojoner <hagentoennies@googlemail.com>
2018-12-20 16:14:09 +0100

    Add gitlab ci file

--- deploy/containerize.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mv ../target/clj-kstream-hh.jar .
-docker build --tag "sojoner/clj-kstream-hh:0.3.0" .
-#docker tag <HASH> sojoner/clj-kstream-hh:0.3.0
-#docker login
-#docker push sojoner/clj-kstream-hh
